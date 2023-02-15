57f442c31245c25b191e9e369fb33d5b7ebfe45a
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-15 17:54:05 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//alert("message");
+// prompt("quel est le nombre ?");
+
+let list = ['john','kokou','abla','cino'];
+console.log(list.forEach(function(ele,index,arr){
+    console.log(ele,index,arr);
+}))
+let i=1;
+while(i<10){
+    console.log(i);
+    i++;
+}
