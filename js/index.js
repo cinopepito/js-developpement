9eff015c3929866f6f6b20920003d531cadedc49
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-02-15 18:03:43 +0100

    file has been added to the index.html

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 //alert("message");
 // prompt("quel est le nombre ?");
 
-let list = ['john','kokou','abla','cino'];
-console.log(list.forEach(function(ele,index,arr){
-    console.log(ele,index,arr);
-}))
-let i=1;
-while(i<10){
-    console.log(i);
-    i++;
+let list = ["john", "kokou", "abla", "cino"];
+console.log(
+  list.forEach(function (ele, index, arr) {
+    console.log(ele, index, arr);
+  })
+);
+let i = 1;
+while (i < 10) {
+  console.log(i);
+  i++;
 }
-for(let i=0;i<20;i++){
-    console.log('number',i)
+for (let i = 0; i < 20; i++) {
+  console.log("number", i);
 }
